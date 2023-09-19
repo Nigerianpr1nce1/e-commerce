6449716904f8c4ee604035dabfc29c20ecaae7ea
st.paul <paulchukwuemeka22@gmail.com>
2023-09-19 21:19:01 +0100

    Fixed issue

--- src/catergories.js
+++ src/catergories.js
@@ -1,196 +1,108 @@
-import "./index.css";
-import { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGreaterThan } from "@fortawesome/free-solid-svg-icons";
+import './index.css'
+import { useState } from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGreaterThan} from '@fortawesome/free-solid-svg-icons';
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
 const Categories = () => {
-  const [isWomenActive, setIsWomenActive] = useState(true);
-  const [isMenActive, setIsMenActive] = useState(true);
-  const [isKidsActive, setIsKidsActive] = useState(true);
-  let arrow = faGreaterThan;
+    const [isWomenActive ,setIsWomenActive] = useState(false)
+    const [isMenActive ,setIsMenActive] = useState(false)
+    const [isKidsActive ,setIsKidsActive] = useState(false)
+    const [womanArrow,setwomanArrow] = useState(faGreaterThan)
+    const [manArrow,setmanArrow] = useState(faGreaterThan)
+    const [kidsArrow,setKidsArrow] = useState(faGreaterThan)
 
-  if ((arrow = "faGreaterThan")) {
-    const newLocal = arrow = "faLessThan";
-  }
-
-  const handleWomenClick = (e) => {
-  setIsWomenActive((current) => !current);
-
-  };
-  if (setIsMenActive(false)) {
-    setIsMenActive((current) => !current);
-  }
-  if (setIsKidsActive(false)) {
-    setIsKidsActive((current) => !current);
-  }
-  const toHandleArrow = () => {
-  };
-  const toggleMenHidden = (e) => {
-    setIsMenActive((current) => !current);
-    if (setIsWomenActive(false)) {
-      setIsWomenActive((current) => !current);
+    const toggleWomenHidden = (e) =>{
+        setIsWomenActive(current => !current)
+        if (setIsMenActive(false)) {
+          setIsMenActive((current) => !current);
+        }
+        if (setIsKidsActive(false)) {
+          setIsKidsActive((current) => !current);
+        }
+        
+        if (womanArrow === faGreaterThan){
+        setwomanArrow (faChevronDown)
+        }
+        if (womanArrow === faChevronDown){
+          setwomanArrow (faGreaterThan)
+          }
     }
-    if (setIsKidsActive(false)) {
-      setIsKidsActive((current) => !current);
-    }
-  };
-  const toggleKidsHidden = (e) => {
-    setIsKidsActive((current) => !current);
-    if (setIsWomenActive(false)) {
-      setIsWomenActive((current) => !current);
-    }
-    if (setIsMenActive(false)) {
-      setIsMenActive((current) => !current);
-    }
-  }
+    const toggleMenHidden =(e) =>{
+        setIsMenActive(current => !current)
+        if (setIsWomenActive(false)) {
+          setIsWomenActive((current) => !current);
+        }
+        if (setIsKidsActive(false)) {
+          setIsKidsActive((current) => !current);
+        }
+        if (manArrow === faGreaterThan){
+          setmanArrow (faChevronDown)
+          }
+          if (manArrow === faChevronDown){
+            setmanArrow (faGreaterThan)
+            }
 
-  return (
-    <>
-      <div className="categories">
-        <div className="phones"></div>
-        <div className="makeup"></div>
-        <p onClick={handleWomenClick}>
-          
-          <FontAwesomeIcon icon={arrow} className="fontarrow" />
-          Women
-        </p>
-        {isWomenActive && (
-          <ul className="Women">
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={arrow} className="fontarrow" /> Shirt
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Sweaters
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Shorts
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Hoodies
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Trousers
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Footwear
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Sleeepwear
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Accesories
-            </li>
-          </ul>
-        )}
-        <p onClick={toggleMenHidden}>
-          <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-          Men
-        </p>
-        {isMenActive && (
-          <ul className="Men">
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Shirt
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Sweaters
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Shorts
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Hoodies
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon
-                icon={faGreaterThan}
-                className="fontarrow"
-              />{" "}
-              Trousers
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Footwear
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Sleeepwear
-            </li>
-            <li>
-              {" "}
-              <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-              Accesories
-            </li>
-          </ul>
-        )}
+    }
+    const toggleKidsHidden = (e) => {
+        setIsKidsActive(current => !current)
+        if (setIsWomenActive(false)) {
+          setIsWomenActive((current) => !current);
+        }
+        if (setIsMenActive(false)) {
+          setIsMenActive((current) => !current);
+        }
+        if (kidsArrow === faGreaterThan){
+          setKidsArrow (faChevronDown)
+          }
+          if (kidsArrow === faChevronDown){
+            setKidsArrow (faGreaterThan)
+            }
 
-        <p onClick={toggleKidsHidden}>Kids</p>
-        <ul className="kids">
-          {isKidsActive && (
-            <ul>
-              <li>
-                {" "}
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Shirt
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Sweaters
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Shorts
-              </li>
-              <li>
-                {" "}
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Hoodies
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Trousers
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Footwear
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Sleeepwear
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faGreaterThan} className="fontarrow" />
-                Accesories
-              </li>
-            </ul>
-          )}
+    }
+
+    return ( <>
+    <div className="categories">
+        <p onClick={toggleWomenHidden}><FontAwesomeIcon icon={womanArrow} className = 'fontarrow' /> Women</p>
+        {isWomenActive && 
+                <ul  className='Women'>
+                    <li> Shirt</li>
+                    <li> Sweaters</li>
+                    <li> Shorts</li>
+                    <li> Hoodies</li>
+                    <li> Trousers</li>
+                    <li> Footwear</li>
+                    <li> Sleepwear</li>
+                    <li> Accessories</li>
+                </ul>}
+        <p onClick={toggleMenHidden}><FontAwesomeIcon icon={manArrow} className = 'fontarrow' /> Men</p>
+        {isMenActive && 
+                <ul  className='Men'>
+                    <li> Shirt</li>
+                    <li> Sweaters</li>
+                    <li> Shorts</li>
+                    <li> Hoodies</li>
+                    <li> Trousers</li>
+                    <li> Footwear</li>
+                    <li> Sleepwear</li>
+                    <li> Accessories</li>
+                </ul>}
+
+        <p onClick={toggleKidsHidden}> <FontAwesomeIcon icon={kidsArrow} className = 'fontarrow' /> Kids</p>
+        <ul className='kids'>
+        {isKidsActive && 
+                <ul>
+                    <li> Shirt</li>
+                    <li> Sweaters</li>
+                    <li> Shorts</li>
+                    <li> Hoodies</li>
+                    <li> Trousers</li>
+                    <li> Footwear</li>
+                    <li> Sleepwear</li>
+                    <li> Accessories</li>
+                </ul>}
         </ul>
-      </div>
-    </>
-  );
-};
+    </div>
+    </> );
+}
+
 export default Categories;
