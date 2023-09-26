c6f698a5d19aebbf671b31536ff24b116e9bf592
st.paul <paulchukwuemeka22@gmail.com>
2023-09-26 17:58:11 +0100

    Commit

--- src/navbar.js
+++ src/navbar.js
@@ -1,4 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.css";
+import { faUserPlus,faCartShopping, faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 const NavBar = () => {
   return (
     <>
@@ -7,8 +9,9 @@ const NavBar = () => {
           <h1 className="logo">Logo</h1>
           <h1 className="name">Name</h1>
           <div className="navbuttons">
-            <h3>Sign In</h3>
-            <h3>Create Account</h3>
+            <h3 className="navicon"> <FontAwesomeIcon icon={faRightToBracket}/> </h3>
+            <h3 className="navicon"> <FontAwesomeIcon icon={faUserPlus}/> </h3>
+            <h3 className="navicon"> <FontAwesomeIcon icon={faCartShopping}/></h3>
           </div>
         </div>
         <div className="nav-sect"></div>
